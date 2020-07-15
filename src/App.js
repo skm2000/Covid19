a174c698adbcafa3092aacf72b452aada70c3f42
skm2000 <shubhamskm2000@gmail.com>
2020-07-15 23:54:41 +0530

    Made Headings and All cases

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import './App.css';
 import Heading from './components/Heading/Heading';
+import AllCases from './components/Cases/AllCases';
 
 const App = () => {
   return(
     <>
      <div className="container-fluid">
      <Heading/>
+     <AllCases/>
      </div>
     </>
   )
--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import ReactStars from "react-rating-stars-component";
-import { Button } from 'react-bootstrap';
+import { Button,Container,Row,Col } from 'react-bootstrap';
 
 const Heading = () => {
     return (
-        <div className="row">
-          <div className="col-sm-6 col-md-8 d-flex justify-content-start">
-            <h2><b>COVID19 GLOBAL</b></h2>
-          </div>
-          <div className="col-sm-6 col-md-4 justify-content-end d-flex">
-             <ReactStars count={5} size={34} activeColor="black"/>
-             <Button variant="outline-dark" size="sm">Submit</Button>
-          </div>
+        <div>
+            <Row>
+              <Col xs={6}><h2><b>COVID19 GLOBAL</b></h2>
+              </Col>
+              <Col xs={6} className="d-md-flex justify-content-end">
+              <ReactStars count={5} size={32} activeColor="red"/>
+                  <Button className="mx-md-2 my-md-2" variant="outline-dark" size="sm">Submit</Button>
+              </Col>
+            </Row>
         </div>
     )
 }
